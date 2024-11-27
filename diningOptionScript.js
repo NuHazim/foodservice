e8f9a52b0f0dc3722fc9c36543c06fe3ae49a8b6
LAI ZI LI <24004555@siswa.um.edu.my>
2024-11-27 15:22:32 +0800

    Added dining options and subsequent pages

---
+++ diningOptionScript.js
@@ -0,0 +1,14 @@
+// Event listeners for navigation
+document.getElementById("dineIn").addEventListener("click", () => {
+    window.location.href = "dinein.html";
+});
+
+document.getElementById("selfPickup").addEventListener("click", () => {
+    window.location.href = "selfpickup.html";
+});
+
+document.getElementById("delivery").addEventListener("click", () => {
+    window.location.href = "delivery.html";
+});
+
+
